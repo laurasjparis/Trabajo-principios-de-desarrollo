e3ad86254a4a4eea662520042481aebb68352095
Zacgreo <160040329+Zacgreo@users.noreply.github.com>
2024-04-26 19:29:28 -0500

    Update Recordatorio.java

---
+++ Recordatorio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calendapp;
+
+/**
+ *
+ * @author user
+ */
+public class Recordatorio {
+    public String enviarRecordatorio(String nombreActividad, int tipoPrioridad, int horaActividad){
+        return "Actividad pendiente :" +nombreActividad+" con priodad "+tipoPrioridad+" programada para las"+horaActividad;
+    }
+    public void compararReloj(int horaActual,int horaActividad){
+        
+    }
+    }
+    
+
